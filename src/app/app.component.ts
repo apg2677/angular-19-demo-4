f0c39ebeceb3df5639903578da295be3bec27adc
apg2677 <apourrezaei@gmail.com>
2025-07-21 14:28:22 -0400

    control flow -  1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { single } from 'rxjs';
+
+interface empInterface {
+  id: number;
+  name: string;
+  role: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [FormsModule, CommonModule],
+  templateUrl: 'app.component.html',
+  styles: ['span {margin:10px ;font-size: 18pt}'],
 })
 export class AppComponent {
-  quantity: number = 1;
-  pricePerItem: number = 100;
-
-  get totalPrice(): number {
-    return this.quantity * this.pricePerItem;
-  }
+  employees = signal<empInterface[]>([
+    // { id: 1, name: 'EmployeeA', role: 'Admin' },
+    // { id: 2, name: 'EmployeeB', role: 'HR' },
+    // { id: 3, name: 'EmployeeC', role: 'Marketing' },
+    // { id: 4, name: 'EmployeeD', role: 'Developer' },
+    // { id: 5, name: 'EmployeeE', role: 'Team Lead' },
+  ]);
 }
