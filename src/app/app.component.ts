6af58cb67efb275f7aafab1d882f7ebe530a70d2
apg2677 <apourrezaei@gmail.com>
2025-07-21 14:36:31 -0400

    ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,11 +17,7 @@ interface empInterface {
   styles: ['span {margin:10px ;font-size: 18pt}'],
 })
 export class AppComponent {
-  employees = signal<empInterface[]>([
-    // { id: 1, name: 'EmployeeA', role: 'Admin' },
-    // { id: 2, name: 'EmployeeB', role: 'HR' },
-    // { id: 3, name: 'EmployeeC', role: 'Marketing' },
-    // { id: 4, name: 'EmployeeD', role: 'Developer' },
-    // { id: 5, name: 'EmployeeE', role: 'Team Lead' },
-  ]);
+  person = {
+    age: 20,
+  };
 }
