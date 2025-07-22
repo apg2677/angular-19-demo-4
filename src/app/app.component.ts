35090447c7943a576f17ab979b04c073cbdf559d
apg2677 <apourrezaei@gmail.com>
2025-07-22 13:22:38 -0400

    text input ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,16 +13,12 @@ interface empInterface {
   selector: 'app-root',
   imports: [FormsModule, CommonModule],
   templateUrl: 'app.component.html',
-  styles: ['span {margin:10px ;font-size: 18pt}'],
+  styleUrl: 'app.component.scss',
 })
 export class AppComponent {
-  employeeRole: string = '';
+  hasText: boolean = false;
 
-  employees = signal<empInterface[]>([
-    { id: 1, name: 'EmployeeA', role: 'Admin' },
-    { id: 2, name: 'EmployeeB', role: 'HR' },
-    { id: 3, name: 'EmployeeC', role: 'Marketing' },
-    { id: 4, name: 'EmployeeD', role: 'Developer' },
-    { id: 5, name: 'EmployeeE', role: 'Team Lead' },
-  ]);
+  textInput(e: any) {
+    this.hasText = e.target.value !== '';
+  }
 }
