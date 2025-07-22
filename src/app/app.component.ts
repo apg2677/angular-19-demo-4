a3cda76d50da9dbd1abfa15372e9b3bf8819536c
apg2677 <apourrezaei@gmail.com>
2025-07-22 13:02:17 -0400

    emp interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { single } from 'rxjs';
 
 interface empInterface {
   id: number;
@@ -17,7 +16,13 @@ interface empInterface {
   styles: ['span {margin:10px ;font-size: 18pt}'],
 })
 export class AppComponent {
-  person = {
-    age: 20,
-  };
+  employeeRole: string = '';
+
+  employees = signal<empInterface[]>([
+    { id: 1, name: 'EmployeeA', role: 'Admin' },
+    { id: 2, name: 'EmployeeB', role: 'HR' },
+    { id: 3, name: 'EmployeeC', role: 'Marketing' },
+    { id: 4, name: 'EmployeeD', role: 'Developer' },
+    { id: 5, name: 'EmployeeE', role: 'Team Lead' },
+  ]);
 }
