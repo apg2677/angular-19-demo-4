82e46ea65acfed998c61929a3096854bc6813aac
apg2677 <apourrezaei@gmail.com>
2025-07-26 16:26:38 -0400

    ng-conatiner - 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,19 +16,18 @@ interface empInterface {
   styleUrl: 'app.component.scss',
 })
 export class AppComponent {
-  nm: string = '';
-  em: string = '';
-  emailIsValid: boolean = false;
-  formSubmitted: boolean = false;
-
-  checkEmailValidation(em: string): void {
-    if (em.includes('@') && em.includes('.com')) {
-      this.emailIsValid = true;
-    } else {
-      this.emailIsValid = false;
-    }
-  }
-  showMessage() {
-    if (this.nm && this.emailIsValid) this.formSubmitted = true;
-  }
+  subjectList = [
+    {
+      subCode: 101,
+      name: 'JavaScript',
+    },
+    {
+      // subCode: 102,
+      name: 'C++',
+    },
+    {
+      subCode: 103,
+      name: 'NestJS',
+    },
+  ];
 }
